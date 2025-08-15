4f6a6029bdce3e80312317ce2f43af57a3304da1
rutinas <ruthshibeshi9@gmail.com>
2025-08-15 12:34:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Load environment variables first
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const authRouter = require('./routers/authrouter');
+
+const app = express();
+
+// -------------------- Middleware --------------------
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//------------------- Database Connection --------------------
+console.log('MONGO_URI from env:', process.env.MONGO_URI);
+
+mongoose.set('strictQuery', true);
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI), {serverSelectionTimeoutMS: 30000};
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Database connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+// -------------------- Routes --------------------
+app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the server' });
+});
+
+// -------------------- Start Server --------------------
+connectDb().then(() => {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
---
+++ models/usersModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        unique: [true, "Email must be unique!"],
+        //minlength: [15, "Email must have at least 15 characters!"],
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password must be provided!"],
+        trim: true,
+        select: false
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationcode: {
+        type: String,
+        select: false,
+    },
+    verificationcodevalidation: {
+        type: String,
+        select: false,
+    },
+    forgotpasswordcode: {
+        type: String,
+        select: false,
+    },
+    forgotpasswordcodevalidation: {
+        type: Number,
+        select: false,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
